a80e2eac96afb57b61ed0f0c672dafe05b958c88
Artyom Lyubeznov <LbzArt@mail.ru>
2015-11-23 00:36:59 +0300

    add util and declare functions

---
+++ src/com/epam/javaUniversity/util/ArrayUtil.java
@@ -0,0 +1,22 @@
+package com.epam.javaUniversity.util;
+
+public final class ArrayUtil {
+    private ArrayUtil() {
+    }
+
+    public static int[] resize(int[] input) {
+        return null;
+    }
+
+    public static boolean equal(int[] first, int second) {
+        return false;
+    }
+
+    public static int[] shuffle(int[] input) {
+        return null;
+    }
+
+    public static void print(int[] input) {
+        
+    }
+}
