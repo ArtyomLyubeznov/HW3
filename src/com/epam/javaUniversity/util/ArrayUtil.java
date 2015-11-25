77e38eca2331ef9d7fff981746fa51723e941b52
Artyom Lyubeznov <LbzArt@mail.ru>
2015-11-25 12:25:02 +0300

    realize function 'equals' and tests for it

--- src/com/epam/javaUniversity/util/ArrayUtil.java
+++ src/com/epam/javaUniversity/util/ArrayUtil.java
@@ -10,13 +10,37 @@ public final class ArrayUtil {
         if (newSize <= 0) {
             return new int[]{};
         }
-        if (input == null){
+        if (input == null) {
             return new int[newSize];
         }
         return Arrays.copyOf(input, newSize);
     }
 
-    public static boolean equal(int[] first, int second) {
+    public static boolean equals(int[] first, int[] second) {
+        if (first == null && second == null) {
+            return true;
+        }
+        if ((first == null && second != null) ||
+                (first != null && second == null)) {
+            return false;
+        }
+        if (first.length != second.length) {
+            return false;
+        }
+        if (matchElements(first, second)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean matchElements(int[] first, int[] second) {
+        int[] firstSort = first.clone();
+        int[] secondSort = second.clone();
+        Arrays.sort(firstSort);
+        Arrays.sort(secondSort);
+        if (Arrays.equals(firstSort, secondSort)) {
+            return true;
+        }
         return false;
     }
 
--- test/com/epam/javaUniversity/util/ArrayUtilTest.java
+++ test/com/epam/javaUniversity/util/ArrayUtilTest.java
@@ -35,4 +35,41 @@ public class ArrayUtilTest {
     public void testResizeNewSizeIsBigger() {
         assertArrayEquals(new int[]{1, 0, 0}, ArrayUtil.resize(new int[]{1}, 3));
     }
+
+    @Test
+    public void testEqualsEmptyArrays() {
+        assertTrue(ArrayUtil.equals(null, null));
+        assertTrue(ArrayUtil.equals(new int[]{}, new int[]{}));
+        assertFalse(ArrayUtil.equals(null, new int[]{}));
+    }
+
+    @Test
+    public void testEqualsOneOfTheArraysIsNull() {
+        assertFalse(ArrayUtil.equals(null, new int[]{1, 2, 3}));
+    }
+
+    @Test
+    public void testEqualsOneOfTheArraysIsEmpty() {
+        assertFalse(ArrayUtil.equals(new int[]{}, new int[]{1, 2, 3}));
+    }
+
+    @Test
+    public void testEqualsArraysHaveDifferentSize() {
+        assertFalse(ArrayUtil.equals(new int[]{1, 2}, new int[]{1, 2, 3}));
+    }
+
+    @Test
+    public void testEqualsArraysHaveDifferentElements() {
+        assertFalse(ArrayUtil.equals(new int[]{1, 2, 3}, new int[]{3, 4, 5}));
+    }
+
+    @Test
+    public void testEqualsArraysHaveElementsInTheSameOrder() {
+        assertTrue(ArrayUtil.equals(new int[]{1, 2, 3}, new int[]{1, 2, 3}));
+    }
+
+    @Test
+    public void testEqualsArraysHaveElementsInTheDifferentOrder() {
+        assertTrue(ArrayUtil.equals(new int[]{1, 2, 3}, new int[]{2, 3, 1}));
+    }
 }
